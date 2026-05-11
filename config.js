630b6c869fd299815f20890aa705549dfec17f6c
me737707-lgtm <me737707@meti.ai>
2026-05-11 14:04:16 +0300

    Update config.js

--- config.js
+++ config.js
@@ -133,6 +133,7 @@ const CONFIG = {
     { username: "Fayez", password: "2468", role: "supervisors", permission: "all" },
     { username: "Asmaa Khaled", password: "123456", role: "Qc", permission: "only" },
     { username: "Wafaa Fathy", password: "123456", role: "Qc", permission: "only" },  
-    { username: "Nesma Mustafa", password: "123456", role: "Qc", permission: "only" }
+    { username: "Nesma Mustafa", password: "123456", role: "Qc", permission: "only" },
+    { username: "Shika", password: "m@1999", role: "Qc", permission: "all" }
     ]
 };
